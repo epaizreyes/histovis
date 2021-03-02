d56c24639faadffe5319ce970b4876d9526652e0
epaizreyes <evelyn.paiz-reyes@ign.fr>
2021-03-02 12:06:31 +0100

    gh-pages-deploy

--- src/cluster.js
+++
@@ -1,3 +0,0 @@
-export { default as HistoricalImage } from './clusters/HistoricalImage';
-export { default as HierarchicalCluster } from './clusters/Cluster';
-export { default as Border } from './clusters/Border';
--- src/photogrammetric-camera.js
+++
@@ -1,6 +0,0 @@
-import * as photogrammetricCamera from 'photogrammetric-camera/dist/photogrammetric-camera';
-
-export { photogrammetricCamera };
-export { default as GoogleOrientationParser } from './parsers/GoogleOrientationParser';
-export { default as lineVS } from './materials/lineVS.glsl';
-export { default as lineFS } from './materials/lineFS.glsl';
